b8238417f2fdaaee549547910d1aede62975e245
[Nevin Mathews] <[nevinmathewsk@gmail.com]>
2022-12-22 18:36:13 +0530

    Updates

---
+++ precache-manifest.09765710c80a746c3d675ee86f595a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d52bc61889c79fc0802c",
+    "url": "/greedygame_analytics/static/css/main.5b1b490c.chunk.css"
+  },
+  {
+    "revision": "d52bc61889c79fc0802c",
+    "url": "/greedygame_analytics/static/js/main.d52bc618.chunk.js"
+  },
+  {
+    "revision": "c9ca29990ee35669108e",
+    "url": "/greedygame_analytics/static/js/1.c9ca2999.chunk.js"
+  },
+  {
+    "revision": "3ca5a15d0a36b1c2483c",
+    "url": "/greedygame_analytics/static/js/runtime~main.3ca5a15d.js"
+  },
+  {
+    "revision": "63207fb8822658a666bbbd2213a44d8d",
+    "url": "/greedygame_analytics/static/media/logo.63207fb8.svg"
+  },
+  {
+    "revision": "478afe195488fafdbbbd55aba2b3c996",
+    "url": "/greedygame_analytics/index.html"
+  }
+];
