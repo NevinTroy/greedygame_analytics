88b99c72467a0d1a3997366b733c7c1c851664a4
[Nevin Mathews] <[nevinmathewsk@gmail.com]>
2022-12-22 06:01:00 +0530

    Refactored Settings

--- src/App.js
+++ src/App.js
@@ -5,51 +5,42 @@ import { requestData, setEnable, setStartDate, setEndDate} from "./actions";
 import Settings from "./components/Settings";
 import "./App.css";
 
-let defaultenableVal=['Clicks', 'AD Requests', 'AD Responses', 'Impressions','Revenue', 'Fill Rate', 'CTR'];
-
 function App(){
-    const dispatch=useDispatch();
+  const dispatch=useDispatch();
 
-    const isPending=useSelector(state=>state.requestData.isPending);
-    const cache_time=useSelector(state=>state.requestData.cache_time);
-    const data=useSelector(state=>state.requestData.data);
-    const appName=useSelector(state=>state.requestData.appName);
-    let startDate=useSelector(state=>state.setStartDate.startDate);
-    let endDate=useSelector(state=>state.setEndDate.endDate);
-    let enableVal=useSelector(state=>state.setEnable.enableVal) ;
-    const error=useSelector(state=>state.requestData.error);
+  const isPending=useSelector(state=>state.requestData.isPending);
+  const cache_time=useSelector(state=>state.requestData.cache_time);
+  const data=useSelector(state=>state.requestData.data);
+  const appName=useSelector(state=>state.requestData.appName);
+  let startDate=useSelector(state=>state.setStartDate.startDate);
+  let endDate=useSelector(state=>state.setEndDate.endDate);
+  let enableVal=useSelector(state=>state.setEnable.enableVal) ;
+  const error=useSelector(state=>state.requestData.error);
 
 
-    useEffect(()=>{
-        requestData(dispatch,'2020-12-11','2020-12-12');
-    },[error])
+  useEffect(()=>{
+      requestData(dispatch,'2020-12-11','2020-12-12');
+  },[error])
 
-    const changeFormat=(date)=>{
-        const d =new Date(date);
-        const nameOfMonth = d.toLocaleString('default', {
-          month: 'long',
-        });
-        return `${d.getDate()} ${nameOfMonth} ${d.getFullYear()}`;
-      }
+  const changeFormat=(date)=>{
+    const d =new Date(date);
+    const nameOfMonth = d.toLocaleString('default', {
+      month: 'long',
+    });
+    return `${d.getDate()} ${nameOfMonth} ${d.getFullYear()}`;
+  }
     
-    const setDate=(event)=>{
-        event.target.id === 'startDate' ? setStartDate(dispatch, event.target.value): setEndDate(dispatch, event.target.value);
-      }
-    
-    const fetchNewData=()=>{
-      requestData(dispatch,startDate,endDate);
-    }
-    return(
-        <div className="App">
-            <div className="bar"></div>
-            <h1>Analytics</h1>
-            <input id='startDate' type='date' value={startDate} onChange={(event)=>{setDate(event)}} />
-            <input id='endDate' type='date' value={endDate} onChange={(event)=>{setDate(event)}} /> 
-            <button onClick={()=>fetchNewData()}>Submit</button>
-        {
-            data === undefined ? <h1>wrong</h1>
-            :
-            <table>
+  const setDate=(event)=>{
+    event.target.id === 'startDate' ? setStartDate(dispatch, event.target.value): setEndDate(dispatch, event.target.value);
+  }
+  return(
+    <div className="App">
+      <div className="bar"></div>
+      <h1>Analytics</h1>
+      <input id='startDate' type='date' value={startDate} onChange={(event)=>{setDate(event)}} />
+      <input id='endDate' type='date' value={endDate} onChange={(event)=>{setDate(event)}} /> 
+      {
+        <table>
           <thead>
             <tr>
               <th>Date</th>
@@ -105,8 +96,8 @@ function App(){
           </tbody>
         </table>
         } 
-        <Settings defaultenableVal={defaultenableVal}/>
-      </div>
+        <Settings setEnable={setEnable} requestData={requestData} startDate={startDate} endDate={endDate} defaultenableVal={enableVal}/>
+    </div>
     )
 }
 
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,6 @@
-import React, {useState, useRef} from 'react'
-import { connect, useDispatch, useSelector, useStore} from 'react-redux';
+import React, {useState} from 'react'
+import { useDispatch} from 'react-redux';
 
-import { setEnable} from "../actions";
 import './Settings.css'
 
 function Settings(props){
@@ -9,6 +8,7 @@ function Settings(props){
   const dispatch=useDispatch();
 
   const [listItems,setListItems]=useState(props.defaultenableVal);
+  const [filteredListItems,setFilteredListItems]=useState(props.defaultenableVal);
 
   const dragItem = React.useRef();
   const dragOverItem = React.useRef();
@@ -22,32 +22,56 @@ function Settings(props){
 
     dragItem.current=null;
     dragOverItem.current=null;
-
+    
     setListItems(_listItems);
-    setEnable(dispatch,_listItems);
+  }
+
+  const handleChange=(e) =>{
+    const { name } = e.target;
+    if (e.target.checked) {
+      setFilteredListItems([...filteredListItems, name]);
+    } else {
+      setFilteredListItems(filteredListItems.filter((item) => item !== name));
+    }
+  }
+
+  function isChecked(item){
+    return filteredListItems.includes(item);
+  }
+
+  const applyChanges= ()=>{
+    const res=listItems.filter(item => filteredListItems.includes(item));
+    props.requestData(dispatch,props.startDate,props.endDate);
+    props.setEnable(dispatch,res);
   }
   return(
-      <div className='container'>
-        <h3>Dimensions and Metrics</h3>
-          {
-            listItems.map((item,index)=>{
-              return (
-                <div 
-                  key={index} 
-                  draggable 
-                  onDragStart={(e)=>dragItem.current=index}
-                  onDragEnter={(e)=>dragOverItem.current=index}
-                  onDragEnd={handleSort}
-                  onDragOver={(e)=>e.preventDefault()} >
-                  <input id="yuh" type='checkbox'/>
-                  <label for="yuh" >{item}</label>
-                </div>
-              )
-            })
-          } 
+    <div className='container'>
+      <h3>Dimensions and Metrics</h3>
+        {
+          listItems.map((item,index)=>{
+            return (
+              <div 
+                key={index} 
+                draggable 
+                onDragStart={(e)=>dragItem.current=index}
+                onDragEnter={(e)=>dragOverItem.current=index}
+                onDragEnd={handleSort}
+                onDragOver={(e)=>e.preventDefault()}
+              >
+                <input 
+                  id={index} name={item} 
+                  type='checkbox' 
+                  checked={isChecked(item)} 
+                  onChange={handleChange}
+                />
+                <label htmlFor={index}>{item}</label> 
+              </div>
+            )
+          })    
+        } 
+      <button onClick={applyChanges}>Apply</button>
     </div>
-    )
-  
+  ) 
 }
 
 export default Settings;
